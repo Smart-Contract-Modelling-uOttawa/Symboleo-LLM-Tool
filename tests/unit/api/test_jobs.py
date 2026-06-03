c0afb6509d773f3e6773c998c89f49306ef6f325
Jeremy <jeremyfang1123@gmail.com>
2026-06-03 14:38:06 -0400

    fix: removed midtest assertions since they violate AAA.

--- tests/unit/api/test_jobs.py
+++ tests/unit/api/test_jobs.py
@@ -6,8 +6,7 @@ from symboleo_llm_tool.api.jobs import TTL, cleanup_expired, create_job, get_job
 def test_cleanup_expired_removes_old_completed_jobs() -> None:
     create_job("old")
     job = get_job("old")
-    assert job is not None
-    job.completed_at = datetime.now() - TTL - timedelta(seconds=1)
+    job.completed_at = datetime.now() - TTL - timedelta(seconds=1)  # type: ignore[union-attr]
     cleanup_expired()
     assert get_job("old") is None
 
@@ -15,8 +14,7 @@ def test_cleanup_expired_removes_old_completed_jobs() -> None:
 def test_cleanup_expired_keeps_recent_completed_jobs() -> None:
     create_job("recent")
     job = get_job("recent")
-    assert job is not None
-    job.completed_at = datetime.now()
+    job.completed_at = datetime.now()  # type: ignore[union-attr]
     cleanup_expired()
     assert get_job("recent") is not None
 
